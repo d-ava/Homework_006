14b0cf75a776b7479ea5f9c70d4056873eb2daa2
d-ava <d.avalishvili@gmail.com>
2021-09-28 20:47:27 +0400

    connected activities using intent and start activity
    data transfer from second to first activity seems to work.
    
    added EditProfileActivity.kt to manifest

--- app/src/main/java/com/example/homework_006/EditProfileActivity.kt
+++ app/src/main/java/com/example/homework_006/EditProfileActivity.kt
@@ -1,5 +1,6 @@
 package com.example.homework_006
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,28 @@ class EditProfileActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+
+        binding.btnSaveProfile.setOnClickListener {
+
+            val name:String = binding.eTName.text.toString()
+            val lastName:String = binding.eTLastName.text.toString()
+            val email:String = binding.eTeMail.text.toString()
+            val dob:String = binding.eTDOB.text.toString()
+            val sex:String = binding.eTSex.text.toString()
+
+            val intentE = Intent(this@EditProfileActivity, MainActivity::class.java)
+
+            intentE.putExtra("name", name)
+            intentE.putExtra("lastName", lastName)
+            intentE.putExtra("email", email)
+            intentE.putExtra("dob", dob)
+            intentE.putExtra("sex", sex)
+
+            startActivity(intentE)
+        }
+
+
     }
 
 
--- app/src/main/java/com/example/homework_006/MainActivity.kt
+++ app/src/main/java/com/example/homework_006/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.homework_006
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.homework_006.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
+
+        binding.btnEditProfile.setOnClickListener {
+
+            val intent = Intent(this@MainActivity, EditProfileActivity::class.java)
+
+            startActivity(intent)
+        }
+
+
+        val ename = intent.getStringExtra("name")
+        if (ename.isNullOrEmpty()){
+            return
+
+        }else binding.tVName.text = ename
+
+
+
+
     }
 }
