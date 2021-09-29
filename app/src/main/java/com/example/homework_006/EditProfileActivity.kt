da260fad5e6d6212af4ced42523464371b0c1c92
d-ava <d.avalishvili@gmail.com>
2021-09-29 08:59:00 +0400

    check if all fields are empty in edit profile activity

--- app/src/main/java/com/example/homework_006/EditProfileActivity.kt
+++ app/src/main/java/com/example/homework_006/EditProfileActivity.kt
@@ -2,7 +2,6 @@ package com.example.homework_006
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.homework_006.databinding.ActivityEditProfileBinding
 import com.example.homework_006.extension.isEmail
@@ -27,19 +26,27 @@ class EditProfileActivity : AppCompatActivity() {
             val dob: String = binding.eTDOB.text.toString()
             val sex: String = binding.eTSex.text.toString()
 
-            val intent = Intent(this@EditProfileActivity, MainActivity::class.java)
 
-            intent.putExtra("name", name)
-            intent.putExtra("lastName", lastName)
 
-            if (email.isEmail()){
+            if (email.isEmail() && !binding.eTName.text.isNullOrBlank() && !binding.eTLastName.text.isNullOrBlank() && !binding.eTDOB.text.isNullOrBlank() && !binding.eTSex.text.isNullOrBlank()) {
+
+
+                val intent = Intent(this@EditProfileActivity, MainActivity::class.java)
+
+                intent.putExtra("name", name)
+                intent.putExtra("lastName", lastName)
+
                 intent.putExtra("email", email)
-            }else binding.eTeMail.error = "wrong email type"
 
-            intent.putExtra("dob", dob)
-            intent.putExtra("sex", sex)
 
-            startActivity(intent)
+                intent.putExtra("dob", dob)
+                intent.putExtra("sex", sex)
+
+                startActivity(intent)
+
+
+            } else binding.tVProfileInformation.text = getString(R.string.empty_fields)
+
         }
 
 
@@ -47,3 +54,31 @@ class EditProfileActivity : AppCompatActivity() {
 
 
 }
+
+/*
+binding.btnSaveProfile.setOnClickListener {
+
+    val name: String = binding.eTName.text.toString()
+    val lastName: String = binding.eTLastName.text.toString()
+    val email: String = binding.eTeMail.text.toString()
+
+    val dob: String = binding.eTDOB.text.toString()
+    val sex: String = binding.eTSex.text.toString()
+
+    val intent = Intent(this@EditProfileActivity, MainActivity::class.java)
+
+    intent.putExtra("name", name)
+    intent.putExtra("lastName", lastName)
+
+    if (email.isEmail() ){
+        intent.putExtra("email", email)
+
+
+
+    }else binding.eTeMail.error = getString(R.string.wrongEmail)
+
+    intent.putExtra("dob", dob)
+    intent.putExtra("sex", sex)
+
+    startActivity(intent)
+}*/
