06d7d0cfe2668c5756f2b08762b242d09d0d5b38
d-ava <d.avalishvili@gmail.com>
2021-09-30 16:54:19 +0400

    isEmail extension was used to check email edit text content on editProfileActivity.kt

--- app/src/main/java/com/example/homework_006/EditProfileActivity.kt
+++ app/src/main/java/com/example/homework_006/EditProfileActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.homework_006.databinding.ActivityEditProfileBinding
+import com.example.homework_006.extension.isEmail
 
 
 class EditProfileActivity : AppCompatActivity() {
@@ -23,18 +24,23 @@ class EditProfileActivity : AppCompatActivity() {
         binding.eTSex.setText(userData?.sex)
 
         binding.btnSaveProfile.setOnClickListener {
-            val userData2 = UserData(
-                name = binding.eTName.text.toString(),
-                lastName = binding.eTLastName.text.toString(),
-                eMail = binding.eTeMail.text.toString(),
-                dob = binding.eTDOB.text.toString(),
-                sex = binding.eTSex.text.toString()
-            )
 
-            val intent2 = Intent()
-            intent2.putExtra(USER2, userData2)
-            setResult(RESULT_OK, intent2)
-            finish()
+            if (binding.eTeMail.text.toString().isEmail()){
+                val userData2 = UserData(
+                    name = binding.eTName.text.toString(),
+                    lastName = binding.eTLastName.text.toString(),
+                    eMail = binding.eTeMail.text.toString(),
+                    dob = binding.eTDOB.text.toString(),
+                    sex = binding.eTSex.text.toString()
+                )
+
+                val intent2 = Intent()
+                intent2.putExtra(USER2, userData2)
+                setResult(RESULT_OK, intent2)
+                finish()
+            }else binding.eTeMail.error = getText(R.string.wrong_email)
+
+
         }
 
     }
